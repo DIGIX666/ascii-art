84223218663bd74cabed9545311877c16b530dc5
DIGIX666 <digixit66@gmail.com>
2022-06-15 17:43:05 +0200

    k

--- main.go
+++ main.go
@@ -24,10 +24,10 @@ func asciiGenerator(text string) {
 	for i := 0; i < len(line); i++ {
 		if len(line[i]) > 0 {
 			chars := []rune(line[i])
-			for n := 0; n < 8; n++ {
+			for n := 0; n < 8; n++ { // vérifie qu'on ne dépasse pas la hauteur de 8 lignes pour les caracts et lettres 
 				for v := 0; v < len(chars); v++ {
 					group := int(chars[v]) - 32
-					adress := group * 9
+					adress := group * 9  // permet de récupper le bon cararct et de les grouper,
 					charLine := content[adress+1+n]
 					fmt.Print(charLine)
 				}
@@ -41,7 +41,7 @@ func asciiGenerator(text string) {
 
 func main() {
 	args := os.Args
-	if len(args) == 2 {
+	if len(args) == 2 { // car dans la ligne de commande ils nous faut juste le nom du fichier + l'argument 
 		asciiGenerator(os.Args[1])
 	} else {
 		fmt.Println("Erreur  nombre d'arguments")
